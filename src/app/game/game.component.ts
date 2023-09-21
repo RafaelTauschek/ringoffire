ee4935c5ca81a550c5dc56e5b53f43f9879aa758
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-09-21 21:18:39 +0200

    card animation and card randomizer, added player component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
-  pickCardAnimation = false;
+export class GameComponent implements OnInit {
+  pickCardAnimation: boolean = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
