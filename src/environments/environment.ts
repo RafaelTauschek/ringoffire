244bad7eb998029a5eee8d8e4ea08528fd4b479b
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-09-24 18:53:16 +0200

    added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD7MxdOD8UbC3ElWEytBB4inYryIpR4KyE",
+    authDomain: "ringoffire-b6202.firebaseapp.com",
+    projectId: "ringoffire-b6202",
+    storageBucket: "ringoffire-b6202.appspot.com",
+    messagingSenderId: "296928119520",
+    appId: "1:296928119520:web:56fb4d663f186492b31e85"
+  },
+};
